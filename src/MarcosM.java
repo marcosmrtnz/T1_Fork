f7b12f7ed647849ee8a3fb3de4e259790fa9e04a
Marcos Marinez <marcos.mrtnz.hdez@gmail.com>
2019-09-11 18:34:55 +0200

    Marcos Martinez

---
+++ src/MarcosM.java
@@ -0,0 +1,5 @@
+public class MarcosM {
+    public static void main(String[] args) {
+        System.out.printf("Seguro que a nadie se le ha ocurrido poner un sout para su primera línea.");
+    }
+}
